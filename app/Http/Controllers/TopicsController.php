f8900402e218f28b77c16854ed8983ac297ab0c1
vagrant <vagrant@homestead>
2018-06-20 06:49:55 +0000

    Author can deletetopic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -57,7 +57,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '编辑成功');
 	}
 
 	public function destroy(Topic $topic)
@@ -65,7 +65,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '删除成功.');
 	}
 
 	public function uploadImage(Request $request, ImageUploadHander $uploader)
