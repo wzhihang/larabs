d23e815c6de7d716034d7761c75d21f1293f77a1
vagrant <vagrant@homestead>
2018-06-20 06:05:53 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="@yield('description', 'laravel爱好者社区')">
     <title>@yield('title', 'larabs') - laravelS</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
