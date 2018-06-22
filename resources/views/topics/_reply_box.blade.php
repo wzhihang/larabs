89cff38a13200ebbdd44b1f94374d7491c55cfb9
vagrant <vagrant@homestead>
2018-06-22 06:51:03 +0000

    User can create reply

--- resources/views/topics/_reply_box.blade.php
+++ resources/views/topics/_reply_box.blade.php
@@ -1,7 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: han
- * Date: 2018/6/20
- * Time: 17:47
- */
+@include('common.error')
+
+<div class="reply-box">
+    <form action="{{ route('replies.store') }}" method="post" accpet-charset="UTF-8">
+        {{ csrf_field() }}
+        <input type="hidden" name="topic_id" value={{ $topic->id }}>
+
+        <div class="form-group">
+            <textarea name="content" class="form-control" placeholder="分享你的想法"  rows="3"></textarea>
+        </div>
+
+        <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-share"></i>回复</button>
+    </form>
+</div>
--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -64,7 +64,8 @@
 
             <div class="panel panel-default topic-reply">
                 <div class="panel-body">
-                    @include('topics._reply_box', ['topic' => $topic])
+                    @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+                    <br>
                     @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
                 </div>
             </div>
