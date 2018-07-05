b130192b460224ddd8e56d66fbd8932f5b9aec48
vagrant <vagrant@homestead>
2018-07-05 08:56:38 +0000

    Sidebar active users

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
@@ -9,12 +10,14 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show( Category $category, Topic $topic, Request $request)
+    public function show( Category $category, Topic $topic, Request $request, User $user)
     {
         $topics = $topic->withOrder($request->order)
             ->where('category_id', $category->id)
             ->paginate(20);
 
-        return view('topics.index', compact(['topics', 'category']));
+        $active_users = $user->getActiveUsers();
+
+        return view('topics.index', compact(['topics', 'category', 'active_users']));
     }
 }
